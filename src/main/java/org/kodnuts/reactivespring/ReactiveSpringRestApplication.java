ab13dd9cff47361acee27b38aa4f3cf1cf8e69aa
Javad Sabbagh <j.sabbagh89@gmail.com>
2018-09-30 15:42:26 +0330

    Create project

---
+++ src/main/java/org/kodnuts/reactivespring/ReactiveSpringRestApplication.java
@@ -0,0 +1,12 @@
+package org.kodnuts.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringRestApplication.class, args);
+	}
+}
